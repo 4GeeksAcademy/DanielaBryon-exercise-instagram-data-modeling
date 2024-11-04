a31604583fb605a20c614395b28ed2488ad820cc
Daniela Bryon <daniellabryren@icloud.com>
2024-11-04 14:01:01 +0000

    Building the corrected Instagram Data Model.

--- src/models.py
+++ src/models.py
@@ -1,32 +1,88 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+{
+    "id":"",
+    "name":"",
+    "posts":[
+        {
+            "user":"",
+            "media":[
+                {
+                    "imagenes",
+                }
+            ]
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+        }
+    ]
+}
 
-    def to_dict(self):
-        return {}
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50),unique=True, nullable=False)
+    firstname = Column(String(50))
+    lastname = Column(String(50))
+    email = Column(String(50), unique=True, nullable=False)
+
+
+    #Relaciones
+
+    posts = relationship('Post', back_populates='user')
+    comments = relationship('Comment', back_populates='author')
+    followers = relationship('Follower', foreign_keys='followers.user_from_id', backref='followers')
+    followings = relationship('Follower', foreign_keys='followers.user_to_id', backref='followings')
+
+class Post(Base):
+    __tablename__ = 'post'
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    #Relaciones
+    user = relationship('User', back_populates='posts')
+    media = relationship('Media', back_populates='post')
+    comments = relationship('Comment', back_populates='post')
+
+class Comment(Base):
+    __tablename__ = 'comment'
+
+    id = Column(Integer, primary_key=True)
+    content = Column(String(250))
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+
+    #Relaciones
+    author = relationship('User', back_populates='comments')
+    post = relationship('Post', back_populates='comments')
+
+class Media(Base):
+    __tablename__ = 'media'
+
+    id = Column(Integer, primary_key=True) 
+    url = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    type = Column(Enum('video', 'imagen', name='media_type'), nullable= False)
+
+    #Realaciones
+    post = relationship('Post', back_populates='media')
+
+class Follower(Base):
+    __tablename__ = 'follower'
+
+    user_from_id = Column(Integer, ForeignKey('user.id'), primary_key=True,)
+    user_to_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+
+
+
 
 ## Draw from SQLAlchemy base
 try:
